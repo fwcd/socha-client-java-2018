76d549570b3b3b526b8f21c1b0fce77573e46b6b
TheDroide <>
2017-10-01 19:51:32 +0200

    Minor formatting changes

--- src/sc/player2018/Starter.java
+++ src/sc/player2018/Starter.java
@@ -20,22 +20,19 @@ import java.io.IOException;
  */
 public class Starter extends AbstractClient {
 
-	private static final Logger	logger = LoggerFactory
-			.getLogger(Starter.class);
-	
-	public Starter(String host, int port, String reservation, String strategy)
-			throws Exception {
+	private static final Logger logger = LoggerFactory.getLogger(Starter.class);
+
+	public Starter(String host, int port, String reservation, String strategy) throws Exception {
 		// client starten
 		super(host, port);
 
 		// strategie auswaehlen und zuweisen
 		IGameHandler logic;
-		
+
 		// Versuche für den strategy-Parameter eine passende Logik zu
 		// instanzieren, sonst verwende Standard
 		try {
-			logic = LogicFactory.valueOf(strategy.toUpperCase()).getInstance(
-					this);
+			logic = LogicFactory.valueOf(strategy.toUpperCase()).getInstance(this);
 		} catch (IllegalArgumentException e) {
 			logic = LogicFactory.DEFAULT.getInstance(this);
 		}
@@ -51,25 +48,22 @@ public class Starter extends AbstractClient {
 
 	}
 
-	public static void main(String[] args) throws IllegalOptionValueException,
-			UnknownOptionException, IOException {
+	public static void main(String[] args) throws IllegalOptionValueException, UnknownOptionException, IOException {
 		System.setProperty("file.encoding", "UTF-8");
 
 		// XXX only for testing
-    // you may use this code to enable debug output:
-    Logger rootLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-    ((ch.qos.logback.classic.Logger)rootLogger).setLevel(ch.qos.logback.classic.Level.WARN);
-    Logger randomlogiclogger = LoggerFactory.getLogger(RandomLogic.class);
-    ((ch.qos.logback.classic.Logger)randomlogiclogger).setLevel(ch.qos.logback.classic.Level.WARN);
+		// you may use this code to enable debug output:
+		Logger rootLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+		((ch.qos.logback.classic.Logger) rootLogger).setLevel(ch.qos.logback.classic.Level.WARN);
+		Logger randomlogiclogger = LoggerFactory.getLogger(RandomLogic.class);
+		((ch.qos.logback.classic.Logger) randomlogiclogger).setLevel(ch.qos.logback.classic.Level.WARN);
 
 		// parameter definieren
 		CmdLineParser parser = new CmdLineParser();
 		CmdLineParser.Option hostOption = parser.addStringOption('h', "host");
 		CmdLineParser.Option portOption = parser.addIntegerOption('p', "port");
-		CmdLineParser.Option strategyOption = parser.addStringOption('s',
-				"strategy");
-		CmdLineParser.Option reservationOption = parser.addStringOption('r',
-				"reservation");
+		CmdLineParser.Option strategyOption = parser.addStringOption('s', "strategy");
+		CmdLineParser.Option reservationOption = parser.addStringOption('r', "reservation");
 
 		try {
 			// Parameter auslesen
@@ -82,10 +76,8 @@ public class Starter extends AbstractClient {
 
 		// Parameter laden
 		String host = (String) parser.getOptionValue(hostOption, "localhost");
-		int port = (Integer) parser.getOptionValue(portOption,
-				SharedConfiguration.DEFAULT_PORT);
-		String reservation = (String) parser.getOptionValue(reservationOption,
-				"");
+		int port = (Integer) parser.getOptionValue(portOption, SharedConfiguration.DEFAULT_PORT);
+		String reservation = (String) parser.getOptionValue(reservationOption, "");
 		String strategy = (String) parser.getOptionValue(strategyOption, "");
 
 		// einen neuen client erzeugen
@@ -93,8 +85,8 @@ public class Starter extends AbstractClient {
 			new Starter(host, port, reservation, strategy);
 		} catch (Exception e) {
 			logger.error("Beim Starten den Clients ist ein Fehler aufgetreten:");
-			//System.err
-			//		.println("Beim Starten den Clients ist ein Fehler aufgetreten:");
+			// System.err
+			// .println("Beim Starten den Clients ist ein Fehler aufgetreten:");
 			e.printStackTrace();
 		}
 
@@ -104,16 +96,14 @@ public class Starter extends AbstractClient {
 		System.out.println();
 		System.out.println(errorMsg);
 		System.out.println();
-		System.out
-				.println("Bitte das Programm mit folgenden Parametern (optional) aufrufen: \n"
-						+ "java -jar mississippi_queen_player.jar [{-h,--host} hostname]\n"
-						+ "                               [{-p,--port} port]\n"
-						+ "                               [{-r,--reservation} reservierung]\n"
-						+ "                               [{-s,--strategy} strategie]");
+		System.out.println("Bitte das Programm mit folgenden Parametern (optional) aufrufen: \n"
+				+ "java -jar mississippi_queen_player.jar [{-h,--host} hostname]\n"
+				+ "                               [{-p,--port} port]\n"
+				+ "                               [{-r,--reservation} reservierung]\n"
+				+ "                               [{-s,--strategy} strategie]");
 		System.out.println();
-		System.out
-				.println("Beispiel: \n"
-						+ "java -jar mississippi_queen_player.jar --host 127.0.0.1 --port 10500 --reservation MQ --strategy RANDOM");
+		System.out.println("Beispiel: \n"
+				+ "java -jar mississippi_queen_player.jar --host 127.0.0.1 --port 10500 --reservation MQ --strategy RANDOM");
 		System.out.println();
 	}
 
@@ -123,8 +113,8 @@ public class Starter extends AbstractClient {
 	}
 
 	@Override
-  public void onGameObserved(String roomId) {
-    // is called when a observation request is acknowledged by the server
-    // this is a newly added method, I am not sure if it fits into the architecture
-  }
+	public void onGameObserved(String roomId) {
+		// is called when a observation request is acknowledged by the server
+		// this is a newly added method, I am not sure if it fits into the architecture
+	}
 }
