f2266fc35823ff92024fd9e65870144eb5f61c4b
fwcd <fwcdmail@gmail.com>
2018-04-25 18:25:37 +0200

    Added simple evaluator

--- src/main/java/com/fwcd/sc18/evaluator/MoveEvaluator.java
+++ src/main/java/com/fwcd/sc18/evaluator/MoveEvaluator.java
@@ -4,6 +4,9 @@ import sc.plugin2018.GameState;
 import sc.plugin2018.Move;
 import sc.shared.PlayerColor;
 
+/**
+ * Evaluates a move on a given GameState.
+ */
 public interface MoveEvaluator {
 	float rate(Move move, PlayerColor myColor, GameState gameBeforeMove, GameState gameAfterMove, boolean wasPruned);
 }
